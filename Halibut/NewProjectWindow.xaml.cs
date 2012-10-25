0bd99a7f2349f2725a5e64efa6a7c64730ac5225
Drew DeVault <sir@cmpwn.com>
2012-10-25 02:20:41 -0600

    Updated exit confirmation

---
+++ Halibut/NewProjectWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Halibut
+{
+    /// <summary>
+    /// Interaction logic for NewProjectWindow.xaml
+    /// </summary>
+    public partial class NewProjectWindow : Window
+    {
+        public NewProjectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
